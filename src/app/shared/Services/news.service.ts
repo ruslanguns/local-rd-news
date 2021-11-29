2b37b1f4a78e5976cc94700eeffffe155d3b371d
Ruslan Gonzalez <ruslanguns@gmail.com>
2021-11-29 18:24:23 +0000

    Take 1 to auto unsubscribe hot observable

--- src/app/shared/Services/news.service.ts
+++ src/app/shared/Services/news.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Observable, throwError } from 'rxjs';
 
-import { catchError, pluck } from 'rxjs/operators';
+import { catchError, pluck, take } from 'rxjs/operators';
 import { Articles } from '../interface/news.interface';
 
 @Injectable({
@@ -20,6 +20,7 @@ export class NewsService {
     return this.http
       .get<{ data: Articles[] }>(`${this.apiUrl}${mediaName}`)
       .pipe(
+        take(1),
         pluck('data'),
         catchError((err) => {
           console.log(err);
