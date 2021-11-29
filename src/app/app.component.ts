5050b644b2652ab0430e6bb9c3e791e39c76c535
Ruslan Gonzalez <ruslanguns@gmail.com>
2021-11-29 18:24:39 +0000

    Added refresh data button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { Articles } from './shared/interface/news.interface';
 import { NewsService } from './shared/Services/news.service';
-import { of, combineLatest } from 'rxjs';
+import { combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 type AvailableDiaries =
@@ -10,6 +9,7 @@ type AvailableDiaries =
   | 'nacional'
   | 'nuevoDiario'
   | 'remolacha';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,16 +17,8 @@ type AvailableDiaries =
 })
 export class AppComponent {
   title = 'local-rd-news';
-  selectedDiary = 'listin'; // cual es la de por defecto?
+  selectedDiary = 'listin';
 
-  // necesitamos un observable q se accione o a lo mejor no! te
-  // muestro lo más facil. pero no se actualizará de momento! luego lo hacemos
-  //  dinamico.
-  // articles$ = this.loadNews('listin');
-
-  /**
-   * Ok ahora vamos a consumirlo y pensar cómo filtrarlo.
-   */
   articlesVm$ = combineLatest([
     this.newsService.getNews('listin'),
     this.newsService.getNews('diarioLibre'),
@@ -43,13 +35,13 @@ export class AppComponent {
     }))
   );
 
-  // articles$ = of([]);
-  // articles: Articles[] = [];
-  // isLoading = false;
-  // hasData = false;
   constructor(private newsService: NewsService) {}
 
   setSelectedDiary(mediaName: AvailableDiaries) {
     this.selectedDiary = mediaName;
+  }
+
+  refreshData() {
+    this.articlesVm$.subscribe();
   }
 }
